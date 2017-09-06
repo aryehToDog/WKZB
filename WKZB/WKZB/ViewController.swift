0cb03a7807b2afdc8f46b7c659767826327c48fe
阿拉斯加的狗 <1281433811@qq.com>
2017-09-06 16:54:30 +0800

    初始化

---
+++ WKZB/WKZB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WKZB
+//
+//  Created by 阿拉斯加的狗 on 2017/9/6.
+//  Copyright © 2017年 阿拉斯加的🐶. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WKZB/WKZBTests/WKZBTests.swift
@@ -0,0 +1,36 @@
+//
+//  WKZBTests.swift
+//  WKZBTests
+//
+//  Created by 阿拉斯加的狗 on 2017/9/6.
+//  Copyright © 2017年 阿拉斯加的🐶. All rights reserved.
+//
+
+import XCTest
+@testable import WKZB
+
+class WKZBTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
